1a4be712217313a9bb5ecd8e268536317bfe659f
Khanh Tran <khanhtran28092000@gmail.com>
2024-08-18 10:11:32 +0000

    Ugly number

---
+++ leetcode/dynamicProgramming/uglyNumber.ts
@@ -0,0 +1,18 @@
+function nthUglyNumber(n: number): number {
+    const dp: number[] = new Array(n);
+    dp[0] = 1;
+    let p2 = 0, p3 = 0, p5 = 0;
+    for (let i = 1; i < n; i++) {
+        dp[i] = Math.min(dp[p2] * 2, dp[p3] * 3, dp[p5] * 5);
+        if (dp[i] === dp[p2] * 2) {
+            p2++;
+        }
+        if (dp[i] === dp[p3] * 3) {
+            p3++;
+        }
+        if (dp[i] === dp[p5] * 5) {
+            p5++;
+        }
+    }
+    return dp[n - 1];
+};
