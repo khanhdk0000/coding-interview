7ea0c47deb8b590a1bf4e67351eeb04ed5cabcef
Khanh Tran <khanhtran28092000@gmail.com>
2025-02-15 15:31:28 +0000

    remove element

---
+++ leetcode/top150/array/removeElement.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # O(n) time, O(1) space
+        
+        # two pointers
+        p = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[p] = nums[i]
+                p += 1
+        return p
