24b8912d2bc318b93e32b931ac73eadb244e1f08
Khanh Tran <khanhtran28092000@gmail.com>
2025-02-16 02:56:22 +0000

    h index

---
+++ leetcode/top150/array/hIndex.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        for i in range(n):
+            if citations[i] >= n - i:
+                return n - i
+        return 0
