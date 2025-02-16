c10366c04011f57d1ddafe1c5efb3922f47cf30e
Khanh Tran <khanhtran28092000@gmail.com>
2025-02-16 10:57:38 +0000

    text justification

--- leetcode/top150/array/textJustification.py
+++ leetcode/top150/array/textJustification.py
@@ -1,25 +1,17 @@
 from typing import List
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        res = []
-        i = 0
-        while i < len(words):
-            j = i + 1
-            line = len(words[i])
-            while j < len(words) and line + len(words[j]) + j - i <= maxWidth:
-                line += len(words[j])
-                j += 1
-            space = maxWidth - line
-            if j - i == 1 or j == len(words):
-                res.append(words[i] + ' ' * space)
-                i = j
-                continue
-            avg, extra = divmod(space, j - i - 1)
-            for k in range(extra):
-                words[i + k] += ' '
-            res.append((' ' * avg).join(words[i:j]))
-            i = j
-        return res
+        res, cur, num_of_letters = [], [], 0
+        for w in words:
+            if num_of_letters + len(w) + len(cur) > maxWidth:
+                for i in range(maxWidth - num_of_letters):
+                    cur[i%(len(cur)-1 or 1)] += ' '
+                res.append(''.join(cur))
+                cur, num_of_letters = [], 0
+            cur += [w]
+            num_of_letters += len(w)
+        return res + [' '.join(cur).ljust(maxWidth)]
 
 sol = Solution()
 print(sol.fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16))
+print(sol.fullJustify(["What","must","be","acknowledgment","shall","be"], 16))
