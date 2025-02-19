36ab3f215ddc7cc919501caeee675817ad3c4a8a
Khanh Tran <khanhtran28092000@gmail.com>
2025-02-19 16:21:02 +0000

    is symmetric

---
+++ leetcode/top150/binaryTree/isSymmetric.py
@@ -0,0 +1,23 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        #iterative
+        if not root:
+            return True
+        stack = [(root.left, root.right)]
+        while stack:
+            left, right = stack.pop()
+            if not left and not right:
+                continue
+            if not left or not right:
+                return False
+            if left.val != right.val:
+                return False
+            stack.append((left.left, right.right))
+            stack.append((left.right, right.left))
+        return True
