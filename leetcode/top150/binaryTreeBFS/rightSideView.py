c48c86b30fffa3db9b708e584ea62a5c3768a886
Khanh Tran <khanhtran28092000@gmail.com>
2025-02-20 03:18:56 +0000

    right side view

---
+++ leetcode/top150/binaryTreeBFS/rightSideView.py
@@ -0,0 +1,27 @@
+from typing import List, Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        res = []
+        stack = [root]
+        while stack:
+            size = len(stack)
+            for i in range(size):
+                node = stack.pop(0)
+                if i == size - 1:
+                    res.append(node.val)
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+        return res
+# time: O(n)
+# space: O(n)
+        
