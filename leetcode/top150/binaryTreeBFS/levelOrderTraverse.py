a5edf259a945ea0397ea5bf0178e29351405354e
Khanh Tran <khanhtran28092000@gmail.com>
2025-02-20 03:35:50 +0000

    level order traverse

---
+++ leetcode/top150/binaryTreeBFS/levelOrderTraverse.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        stack = [root]
+        while stack:
+            size = len(stack)
+            level = []
+            for i in range(size):
+                node = stack.pop(0)
+                level.append(node.val)
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+            res.append(level)
+        return res
