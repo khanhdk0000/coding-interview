46066feb704eebd6cfd5b5c48db0195e1734a83a
Khanh Tran <khanhtran28092000@gmail.com>
2025-04-12 14:58:41 +0000

    findintersection

--- coderbyte/findintersection.py
+++ coderbyte/findintersection.py
@@ -1,28 +1,24 @@
 def FindIntersection(strArr):
+  arrayA = [int(x.strip()) for x in strArr[0].split(",")]
+  arrayB = [int(x.strip()) for x in strArr[1].split(",")]
 
-  # code goes here
-  firstArr = [int(x) for x in strArr[0].split(",")]
-  secondArr = [int(x) for x in strArr[1].split(",")]
+  i, j = 0, 0
+  intersection = []
+  while i < len(arrayA) and j < len(arrayB):
+    if arrayA[i] == arrayB[j]:
+      intersection.append(str(arrayA[i]))
+      i += 1
+      j += 1
+    elif arrayA[i] < arrayB[j]:
+      i += 1
+    else:
+      j += 1
+  if intersection:
+    return ','.join(intersection)
+  else:
+    return "false"
 
-  lenFirst = len(firstArr)
-  lenSecond = len(secondArr)
-  idx1, idx2 = 0, 0
-  res = []
-  while idx1 < lenFirst and idx2 < lenSecond:
-    if firstArr[idx1] == secondArr[idx2]:
-      res.append(firstArr[idx1])
-      idx1 += 1
-      idx2 += 1
-    elif firstArr[idx1] < secondArr[idx2]:
-      idx1 += 1
-    elif firstArr[idx1] > secondArr[idx2]:
-      idx2 += 1
-
-  return ','.join(str(i) for i in res) if len(res) > 0 else "false"
 
 # keep this function call here 
 input = ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
 print(FindIntersection(input))
-
-# "9???1???9???1???9"
-# 5??aaaaaaaaaaaaaaaaaaa?5?a??5"
