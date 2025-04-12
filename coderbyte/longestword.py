fb2af7077232c1d8dac9998966028c2c42884c07
Khanh Tran <khanhtran28092000@gmail.com>
2025-04-12 16:54:58 +0000

    longest word

--- coderbyte/longestword.py
+++ coderbyte/longestword.py
@@ -1,29 +1,17 @@
 def LongestWord(sen):
 
-  # code goes here
-	res = ""
-	count = 0
-	maxCount = 0
-	left, right = 0, 0
-	for i in range(len(sen)):
-		if sen[i].isalpha():
-			if count == 0:
-				count += 1
-				left = i
-				right = i
-			else:
-				count += 1
-				right += 1
-		else:
-			if count > maxCount:
-				res = sen[left:right+1]
-				maxCount = count
-			count = 0
-	if count > maxCount:
-		res = sen[left:right+1]
-		maxCount = count
-	# print(maxCount)
-	return res
+  words = sen.split(' ')
+  result = ''
+  count = 0
+  for word in words:
+    localCount = 0
+    for c in word:
+      if c.isalnum():
+        localCount += 1
+    if localCount > count:
+      count = localCount
+      result = word  
+  return result
 
 # keep this function call here 
 input = "fun&!! time"
