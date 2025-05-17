8f635625e337dc6be0113c63f1f10ebf782c9368
Khanh Tran <khanhtran28092000@gmail.com>
2025-05-17 09:23:00 +0000

    is palindrome

--- leetcode/top150/array/isPalindrome.py
+++ leetcode/top150/array/isPalindrome.py
@@ -3,3 +3,15 @@ class Solution:
         s = s.lower()
         s = ''.join([i for i in s if i.isalnum()])
         return s == s[::-1]
+
+    def isPalindrome2(self, s: str) -> bool:
+        # sliding window, 2 pointers
+        s = s.lower()
+        s = ''.join([i for i in s if i.isalnum()])
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
