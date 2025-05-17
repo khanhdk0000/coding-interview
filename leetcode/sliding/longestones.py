d5a4dc1bfd26b042f9b1b0381b7d93f5d25406bc
Khanh Tran <khanhtran28092000@gmail.com>
2025-05-17 09:15:44 +0000

    longest ones

--- leetcode/sliding/longestones.py
+++ leetcode/sliding/longestones.py
@@ -2,17 +2,18 @@ from typing import List
 
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        i = 0
-        j = 0
-        for j in range(len(nums)):
-            # print(i, j, k)
-            if nums[j] == 0:
+        left, right = 0, 0
+        maxLength = 0
+        while right < len(nums):
+            if nums[right] == 0:
                 k -= 1
-            if k < 0:
-                if nums[i] == 0:
+            while k < 0:
+                if nums[left] == 0:
                     k += 1
-                i += 1
-        return j - i + 1
+                left += 1
+            maxLength = max(maxLength, right - left + 1)
+            right += 1
+        return maxLength
 
 
 sol = Solution()
