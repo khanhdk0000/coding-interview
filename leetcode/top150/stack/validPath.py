785affe6d6cfefa10d2c79a84d3c0bfcba9e0f4c
Khanh <khanhtran28092000@gmail.com>
2026-04-20 23:54:28 +0700

    Update validpath

--- leetcode/top150/stack/validPath.py
+++ leetcode/top150/stack/validPath.py
@@ -1,10 +1,26 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         stack = []
-        for p in path.split("/"):
-            if p == "..":
+        i = 0
+        n = len(path)
+
+        while i < n:
+            # Skip consecutive slashes
+            while i < n and path[i] == '/':
+                i += 1
+
+            # Read the next component
+            component = []
+            while i < n and path[i] != '/':
+                component.append(path[i])
+                i += 1
+
+            part = "".join(component)
+
+            if part == "..":
                 if stack:
                     stack.pop()
-            elif p and p != ".":
-                stack.append(p)
+            elif part and part != ".":
+                stack.append(part)
+
         return "/" + "/".join(stack)
