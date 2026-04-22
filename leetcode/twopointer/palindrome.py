a4e0e9b8260aead3e7564e972eb3a8f66963b955
Khanh <khanhtran28092000@gmail.com>
2026-04-22 11:25:22 +0700

    is palindrome 2

--- leetcode/twopointer/palindrome.py
+++ leetcode/twopointer/palindrome.py
@@ -30,6 +30,19 @@ class Solution:
         palin = ''.join(filter(lambda x: x.isalnum(), s)).lower()
         return palin == palin[::-1]
 
+    def isPalindrome2(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+
 s = "A man, a plan, a canal: Panama"
 # s = "race a car"
 # s = " "
