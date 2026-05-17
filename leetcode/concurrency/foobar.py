d00c41af88df5b25e4126154203527790baa13fd
Khanh <khanhtran28092000@gmail.com>
2026-05-17 23:01:06 +0700

    update foobar

--- leetcode/concurrency/foobar.py
+++ leetcode/concurrency/foobar.py
@@ -1,19 +1,25 @@
+import threading
 class FooBar:
     def __init__(self, n):
         self.n = n
+        self.sem1 = threading.Semaphore(0)
+        self.sem2 = threading.Semaphore(1)
 
 
     def foo(self, printFoo: 'Callable[[], None]') -> None:
         
         for i in range(self.n):
             
+            self.sem2.acquire()
             # printFoo() outputs "foo". Do not change or remove this line.
             printFoo()
+            self.sem1.release()
 
 
     def bar(self, printBar: 'Callable[[], None]') -> None:
         
         for i in range(self.n):
-            
+            self.sem1.acquire()
             # printBar() outputs "bar". Do not change or remove this line.
             printBar()
+            self.sem2.release()
