657c7f405fe6260b335fdc42439d9da6b840330d
Khanh <khanhtran28092000@gmail.com>
2026-09-02 12:39:34 +0700

    invertTreeIterative

--- leetcode/tree/invertTree.py
+++ leetcode/tree/invertTree.py
@@ -6,3 +6,16 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+
+    def invertTreeIterative(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return root
