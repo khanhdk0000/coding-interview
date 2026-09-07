bdd2aeb1c8e8a1528f32b310daa979934fdb3188
Khanh <khanhtran28092000@gmail.com>
2026-09-07 07:43:34 +0700

    isValidBST

---
+++ leetcode/tree/bstValidation.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.is_within_bounds(root, float('-inf'), float('inf'))
+    def is_within_bounds(self, node: Optional[TreeNode], lower_bound: int, upper_bound: int) -> bool:
+        if not node:
+            return True
+        if not lower_bound < node.val < upper_bound:
+            return False
+        if not self.is_within_bounds(node.left, lower_bound, node.val):
+            return False
+        return self.is_within_bounds(node.right, node.val, upper_bound)
